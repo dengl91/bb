2ac614b2ea43b69fe7a3172aa86159c4ef0a5493
dengl91 <denisflip@gmail.com>
2021-04-08 13:19:14 +0300

    Create homepage for desktop

---
+++ public/js/main.js
@@ -0,0 +1,67 @@
+// custom script
+
+(function($) {
+    $(document).ready(function() {
+
+        // Counter
+        $(window).scroll( function() {
+            $('[data-count]').each(function () {
+                let target = $(this);
+                if ( isOnScreen(target) && !target.hasClass('counted') ) {
+                    countUp(target);
+                    target.addClass('counted');
+                }
+            });
+            $('[data-width]').each(function () {
+                let target = $(this);
+                if ( isOnScreen(target) && !target.hasClass('counted') ) {
+                    setWidth(target);
+                    target.addClass('counted');
+                }
+            });
+        });
+
+        document.addEventListener('click', function(e) {
+            var map = document.querySelector('#map-wrap iframe')
+            if ( e.target.id === 'map-wrap' ) {
+                map.style.pointerEvents = 'all'
+            } else {
+                map.style.pointerEvents = 'none'
+            }
+        });
+
+        function countUp(target) {
+            let defaultCount = parseFloat(target.text());
+            let totalCount = target.data('count');
+            let increaser = Math.floor(totalCount / 20);
+            defaultCount = defaultCount + increaser;
+            if ( totalCount >= defaultCount ) {
+                target.text(parseFloat(defaultCount).toFixed());
+                setTimeout(() => {
+                    countUp(target);
+                }, 100);
+            } else {
+                return;
+            }
+        }
+
+        function isOnScreen(elem) {
+            if ( elem.length == 0 ) {
+                return;
+            }
+            var $window = $(window)
+            var viewport_top = $window.scrollTop()
+            var viewport_height = $window.height()
+            var viewport_bottom = ( viewport_top + viewport_height ) * 1.1
+            var $elem = $(elem)
+            var top = $elem.offset().top
+            var height = $elem.height()
+            var bottom = top + height
+        
+            return (top >= viewport_top && top < viewport_bottom) ||
+            (bottom > viewport_top && bottom <= viewport_bottom) ||
+            (height > viewport_height && top <= viewport_top && bottom >= viewport_bottom)
+        }
+
+    });
+})( jQuery );
